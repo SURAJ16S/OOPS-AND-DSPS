a4482e4f07842dce75194ce955fa4e073d80060e
Mr.BugSploiter <62634504+SURAJ16S@users.noreply.github.com>
2024-08-01 22:11:42 +0530

    Update DSPS-assignment1-Linear_Search.cpp

--- DSPS-assignment1-linear_search.cpp
+++ DSPS-assignment1-linear_search.cpp
@@ -1,44 +1,74 @@
 #include<iostream>
 using namespace std;
 
-void count(int arr[],int n,int target){
-    int count = 0, i=0;
-    for(i=0;i<n;i++){
-        if(arr[i]==target){
-            count+=1;
-
+class linsearch{
+    public:
+    int arr[10] , n , found =0 , i , target , count = 0;
+    int lfound = 0;
+    
+    void accept(){
+        cout<<"Enter size of array :";
+        cin>>n;
+        
+        for(i=0;i<n;i++){
+            cout<<"Enter "<<i<<" th Element : ";
+            cin>>arr[i];
+        }
+        
+        cout<<"Enter Element To Search : ";
+        cin>>target;    
+    }
+    
+    void search(){
+        for(i=0;i<n;i++){
+            if(arr[i]==target){
+                found = 1;
+                break;
+            }
+        }
+        if(found){
+            cout<<"\nElement "<<target<<" Present At "<<i<< " Position";
+        }
+        else{
+            cout<<"\nElement Not Present";
+        }
+        
+        //count
+        for(i=0;i<n;i++){
+            if(arr[i]==target){
+                count+=1;
+            }
+        }
+        if(count>1){
+            cout<<"\nElement "<<target<<" Repeated "<<count<<" Times";
+        } 
+        else if(count==1){
+            cout<<"\nElement "<<target<<" Does Not Repeat";
+        }
+        else{
+            cout<<"\nElement "<<target<<" Repeats Only Once ";
         }
     }
-    cout<<"Element "<<target<<" is repeated "<<count<<" Times";
-}
+    
+    int lastOccur(){
+        for(i=n-1;i>=0;i--){
+            if(arr[i]==target){
+                lfound=1;
+                break;
+            }
+        }
+        if(lfound){ 
+            cout<<"\nLast Occurrence Of "<<target<<" Is At Position "<<i;
+        }
+        return i;
+    }
+    
+};
 
 int main(){
-    int arr[10];
-    int n , i=0 , target ,flag=0;
-    cout<<"Enter Array Size :";
-    cin>>n;
-    for(i=0;i<n;i++){
-        cout<<"Enter "<<i<<" Position Element :";
-        cin>>arr[i];
-    }
-    cout<<"Enter Element To Search :";
-    cin>>target;
-    
-    for(i=0;i<n;i++){
-        if(arr[i]==target){
-            flag = 1;
-            break;
-        }
-    }
-    
-    if(flag){
-        cout<<"Element "<<target<<" Present At "<<i<<" Position"<<endl;
-    }
-    else{
-        cout<<"Element "<<target<<" Not Present";
-    }
-    
-   count(arr,n,target);
-    
+    linsearch test;
+    test.accept();
+    test.search();
+    test.lastOccur();
     return 0;
 }
